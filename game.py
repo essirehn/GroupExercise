493614eae1ea1db92d362c5a51e2c1dae6930bcc
essirehn <ext-essi.rehn@qentinel.com>
2020-09-21 14:48:33 +0300

    while looppi luotu

---
+++ game.py
@@ -0,0 +1,9 @@
+game_on = True
+
+def game():
+    aloitustekstit jyriltä
+    while == True:
+        for kysymys in kysymykset:
+            laske tulos_määrä
+        print tuloks_määrä #lopullinen tulos
+        game_on = False
