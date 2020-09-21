f8e6f0c3ba9fae79267c3c104aba5deb523bfffb
TuomasAalto <ext-tuomas.aalto@qentinel.com>
2020-09-21 14:42:57 +0300

    kysymykset

---
+++ game.py
@@ -0,0 +1,4 @@
+
+kysymykset = {"Maila ja pallo maksavat yhteensä 1,10 euroa. Maila maksaa 1 euron enemmän kuin pallo. Kuinka paljon pallo maksaa?", "5 senttiä"
+...          "Jos viidellä laitteella kestää viisi minuuttia valmistaa viisi tuotetta, kuinka kauan sadalta koneelta kestää valmistaa sata tuotetta?", "5 minuuttia"
+...          "Järvi on täynnä lumpeenlehtiä. Joka päivä lumpeiden peittämä alue kaksinkertaistuu. Jos lumpeet täyttävät koko järven 48 päivässä, kuinka kauan kestää, että järvi on peittynyt puolilleen?", "47 päivää"}
